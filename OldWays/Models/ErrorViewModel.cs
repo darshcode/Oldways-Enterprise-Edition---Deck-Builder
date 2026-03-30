b09a5abeed505f0f28840b93dc79d67c06ef89f6
darshmeen D <darsh.dhillon@live.com>
2026-03-29 20:25:17 -0400

    ASP.NET MVC with Identity setup

---
+++ OldWays/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OldWays.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
