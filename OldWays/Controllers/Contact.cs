2ebd963f01573a58a70d07794a2e6cd5a5bfc722
darshmeen D <darsh.dhillon@live.com>
2026-06-27 14:50:19 -0400

    static contacts page added

---
+++ OldWays/Controllers/Contact.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using OldWays.Models;
+
+namespace OldWays.Controllers
+{
+    public class Contact : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
